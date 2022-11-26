d332f7e95d8d49b15af10cd7f1accfa748741f30
cernockyd <24832873+cernockyd@users.noreply.github.com>
2022-11-26 22:52:57 +0100

    register ping

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,39 @@
-import { Client, GatewayIntentBits, Partials } from "discord.js";
+import { REST, Routes, Client, GatewayIntentBits, Partials } from "discord.js";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-if (!process.env.DISCORD_BOT_TOKEN) throw new Error("Missing bot token.");
+if (!process.env.DISCORD_BOT_TOKEN)
+  throw new Error("Missing DISCORD_BOT_TOKEN.");
+if (!process.env.DISCORD_APP_CLIENT_ID)
+  throw new Error("Missing DISCORD_APP_CLIENT_ID.");
+
+const commands = [
+  {
+    name: "ping",
+    description: "Replies with Pong!",
+  },
+];
+
+const rest = new REST({ version: "10" }).setToken(
+  process.env.DISCORD_BOT_TOKEN
+);
+
+(async () => {
+  try {
+    console.log("Started refreshing application (/) commands.");
+
+    await rest.put(
+      /* eslint-disable  @typescript-eslint/no-non-null-assertion */
+      Routes.applicationCommands(process.env.DISCORD_APP_CLIENT_ID!),
+      { body: commands }
+    );
+
+    console.log("Successfully reloaded application (/) commands.");
+  } catch (error) {
+    console.error(error);
+  }
+})();
 
 const client = new Client({
   intents: [
